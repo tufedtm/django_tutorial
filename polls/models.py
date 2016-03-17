bdebabb356f6b14126fc6232c7863d95e8ea1f0a
tufedtm <tufedtm@gmail.com>
2016-03-17 17:44:54 +0300

    + tutorial 01

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,26 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField('Текст вопроса', max_length=200)
+    created = models.DateTimeField('Дата создания', auto_now_add=True)
+
+    def __str__(self):
+        return self.question_text
+
+    def was_created_recently(self):
+        print(self.created)
+        print(timezone.now())
+        print(datetime.timedelta(days=1))
+        return self.created >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, verbose_name='Вопрос')
+    choice_text = models.CharField('Текст ответа', max_length=200)
+    votes = models.IntegerField('Количество голосов', default=0)
+
+    def __str__(self):
+        return self.choice_text
